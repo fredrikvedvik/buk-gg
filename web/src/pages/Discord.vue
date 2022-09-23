5aa8cd2163b79d653845bf242fee3cd52603ef76
Fredrik Vedvik <fredrik@vedvik.tech>
2022-09-23 11:48:33 +0200

    some style

--- web/src/pages/Discord.vue
+++ web/src/pages/Discord.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="bg-slate-700 p-4 rounded rounded-lg">
+    <div class="flex flex-col bg-slate-700 p-4 rounded rounded-lg gap-4">
         <h1 class="text-lg">Linked accounts</h1>
-        <div v-for="discord in settings.discords" class="flex bg-slate-800 p-2 rounded rounded-lg px-4">
-            <h1 class="my-auto">{{discord.username}}#{{discord.discriminator}}</h1>
-            <XMarkIcon class="h-8 w-8 ml-auto fill-red-300 hover:-translate-y-0.5 transition duration-200 cursor-pointer" @click="removeDiscord(discord.id)"></XMarkIcon>
+        <div class="flex flex-col gap-2">
+            <div v-for="discord in settings.discords" class="flex bg-slate-800 p-2 rounded rounded-lg px-4">
+                <h1 class="my-auto">{{discord.username}}#{{discord.discriminator}}</h1>
+                <XMarkIcon class="h-8 w-8 ml-auto fill-red-300 hover:-translate-y-0.5 transition duration-200 cursor-pointer" @click="removeDiscord(discord.id)"></XMarkIcon>
+            </div>
         </div>
         <button @click="login" class="mt-2 bg-green-500 p-2 rounded rounded-lg px-3" v-if="!settings.discords || settings.discords.length <= 1">Add</button>
+        <p class="text-gray-500" v-if="settings.discords?.length >= 2">You can only have 2 accounts connected to your account</p>
     </div>
 </template>
 <script lang="ts" setup>
